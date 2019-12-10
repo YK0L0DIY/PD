fe006592b4a0daf01555ee550858565b03849732
yk0l0diy <ykolodiy17@gmail.com>
2019-12-10 22:45:14 +0000

    new version correct

--- projeto final/teste.pl
+++ projeto final/teste.pl
@@ -6,9 +6,9 @@ nonogram(RConstr, CConstr, Rows) :-
     append(Rows, Cols, Lines),
     solve(Lines, LineConstr).
 
-/**
- * matrix return the Cols and the Rows
- */
+%
+% matrix devolve as Rows Cols da matrix
+%
 
 matrix(NRows, NCols, Rows, Cols) :-
     create_mat(NRows, NCols, Rows),
@@ -30,56 +30,51 @@ extract_columns(N, Max, Rows, [Col|Cols]) :-
     extract(Rows,Col, InvN),
     N1 is N - 1,
     extract_columns(N1, Max, Rows, Cols).
+
 extract(Rows, Col, N) :-
     maplist(nth1(N), Rows, Col).
 
 %
-% Nonogram solving algorithm
+% Algoritmo para resolver o nonograma
 %
 
 solve(Lines, Constrs) :-
-    pack(Lines, Constrs, Pack),     % todas as possibilidades por linha/coluna
-    sort(Pack, SortedPack),         % ordena por ordem de menores possibilidades
-    solve(SortedPack).              % consroi as restricoes fixando uma a uma
+    allPossibilitis(Lines, Constrs, All),     % todas as possibilidades por linha/coluna
+    sort(All, Sorted),                        % ordena por ordem de menores possibilidades
+    solve(Sorted).                            % verefica se a linha esta correta relativamente as possibilidades
 
 solve([]).
 solve([line(_, Line, Constr)|Rest]) :-
     check_line(Line, Constr),
     solve(Rest).
 
-/**
- * pack(+Lines:list, +Constrs:list, -Result:list) is det.
- *
- * Packs a line and its constraints into a single term and adds the number of
- * possible line solutions given the line's length and constraints as the term's
- * first argument to enable sorting.
- */
-pack([], [], []).
-pack([Line|Lines], [Constr|Constrs], [line(Count, Line, Constr)|Result]) :-
+%
+%   calcula todas a possibilidades uma linha segundo as restricoes e devolve uma lista de tupulos
+%   cada tupulo com o numero de possibilidades para essa linha, linha e contricao criada
+%
+
+allPossibilitis([], [], []).
+allPossibilitis([Line|Lines], [Constr|Constrs], [line(Count, Line, Constr)|Result]) :-
     length(Line, LineLength),
     length(CheckLine, LineLength),
     findall(CheckLine, check_line(CheckLine, Constr), NCheckLine),
     length(NCheckLine, Count),
-    pack(Lines, Constrs, Result).
+    allPossibilitis(Lines, Constrs, Result).
 
-/**
- * check_line(+Line:list ,+Constraints:list) is nondet.
- *
- * Checks if the given Line satisfies the Constraints. Can also generate all
- * valid lines if given a line with some or all members unbound. Examples:
- *   ?- check_line([x, ' ', x, ' '], [1,1]).
- *   true .
- *   ?- L = [_,_,_,_,_], check_line(L, [2,1]).
- *   L = [x, x, ' ', x, ' '] ;
- *   L = [x, x, ' ', ' ', x] ;
- *   L = [' ', x, x, ' ', x] .
- */
+
+% check_line verefica se a linha esta correta segundo as constricoes ou constroi uma linha segundo as restricoes
+%   Exemplo:
+%   ?- L = [_,_,_,_,_], check_line(L, [2,1]).
+%   L = [x, x, ' ', ' ', x] ;
+%   L = [' ', x, x, ' ', x] ;
+%   L = [x, x, ' ', x, ' '] .
+
 check_line([],[]) :- !.
 check_line(Line, [Part|Rest]) :-
     Rest \= [],
     add_space(Line, Line2),
     check_part(Line2, Line3, Part),
-    force_space(Line3, Line4),
+    space(Line3, Line4),
     check_line(Line4, Rest).
 check_line(Line, [Part|[]]) :-
     add_space(Line, Line2),
@@ -87,37 +82,30 @@ check_line(Line, [Part|[]]) :-
     add_space(Line3, Line4),
     check_line(Line4, []).
 
-force_space([' '|Line],Line).
+space([' '|Line],Line).                 % adiciona um espaco
 
-add_space(Line, Line).
+add_space(Line, Line).                  % verefica se precisa de um espaco
 add_space([' '|Line],RestLine) :-
     add_space(Line, RestLine).
 
-check_part(Line, Line, 0).
-check_part(['x'|Line], RestLine, N) :-
+check_part(Line, Line, 0).              % verifica se a linha esta com as partes cprretas
+check_part(['x'|Line], Rest, N) :-
     N > 0,
     N1 is N - 1,
-    check_part(Line, RestLine, N1).
+    check_part(Line, Rest, N1).
 
 %
-% Functions to produce a nice output
+% Predicados para fazer print
 %
 
-print_solution([]).
-print_solution([Row|Rows]) :-
-    print(' '),
-    print_line(Row),
-    nl,
-    print_solution(Rows).
+print_elements([]).             % print de um elemento
+print_elements([Element|T]):-
+    Element=' ', write(' .'),print_elements(T).
+print_elements([Element|T]):-
+    Element='x', write(' X'),print_elements(T).
 
-print_line([]).
-print_line([Var|Vars]) :-
-    print_var(Var),
-    print(' '),
-    print_line(Vars).
-
-print_var(' ') :- print('.').
-print_var('x') :- print('X').
+print_rows([]).               % print da matrix
+print_rows([H|T]) :- print_elements(H), nl, print_rows(T).
 
 %
 % Tests and examples
@@ -148,10 +136,24 @@ test4 :-
     puzzle([RowConstr, ColConstr]).
 
 test5 :-
-     RowConstr = [[1],[3],[1,1,1],[1],[1]],
-     ColConstr = [[1],[1],[5],[1],[1]],
+     RowConstr = [[1,1], [2,1,2], [2,1,1,2], [5,1,1,5], [4,1,1,5], [3,2,4], [3,4,4],
+                 [11], [4], [3,4,3], [10], [6,2], [11], [2,6], [10], [2,4,2], [4],
+                 [2,2], [8], [2,1,1,2], [2,2,2,2], [4,4], [8]],
+     ColConstr = [[2], [5,1,1], [7,1,2,1,1], [5,2,1,2,3], [3,2,1,1,1,2,2], [1,5,2,3],
+                 [2,17], [12,1,1], [12,1,1], [2,17], [1,5,2,3], [2,1,1,1,2,2],
+                 [6,2,1,2,3], [5,1,1,1,1], [7,1,1], [5], [2]],
+     puzzle([RowConstr, ColConstr]).
+
+test6 :-
+     RowConstr = [[6], [3, 1, 3], [1, 3, 1, 3], [3, 14], [1, 1, 1], [1, 1, 2, 2], [5, 2, 2], [5, 1, 1], [5, 3, 3, 3], [8, 3, 3, 3]],
+     ColConstr = [[4], [4], [1, 5], [3, 4], [1, 5], [1], [4, 1], [2, 2, 2], [3, 3], [1, 1, 2], [2, 1, 1], [1, 1, 2], [4, 1], [1, 1, 2], [1, 1, 1], [2, 1, 2], [1, 1, 1], [3, 4], [2, 2, 1], [4, 1]],
+     puzzle([RowConstr, ColConstr]).
+
+test7 :-
+     RowConstr = [[5], [2, 3, 2], [2, 5, 1], [2, 8], [2, 5, 11], [1, 1, 2, 1, 6], [1, 2, 1, 3], [2, 1, 1], [2, 6, 2], [15, 4], [10, 8], [2, 1, 4, 3, 6], [17], [17], [18], [1, 14], [1, 1, 14], [5, 9], [8], [7]],
+     ColConstr = [[5], [3, 2], [2, 1, 2], [1, 1, 1], [1, 1, 1], [1, 3], [2, 2], [1, 3, 3], [1, 3, 3, 1], [1, 7, 2], [1, 9, 1], [1, 10], [1, 10], [1, 3, 5], [1, 8], [2, 1, 6], [3, 1, 7], [4, 1, 7], [6, 1, 8], [6, 10], [7, 10], [1, 4, 11], [1, 2, 11], [2, 12], [3, 13]],
      puzzle([RowConstr, ColConstr]).
 
 puzzle([RowConstr, ColConstr|_]) :-
     nonogram(RowConstr, ColConstr, Rows),
-    nl, print_solution(Rows), nl.
+    nl, print_rows(Rows), nl.
