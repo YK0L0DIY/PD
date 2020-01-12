c9775911bf12b73307670bd582fb00054666d372
yk0l0diy <ykolodiy17@gmail.com>
2020-01-12 15:03:31 +0000

    new version of the work

--- projeto final/final.pl
+++ projeto final/final.pl
@@ -4,6 +4,7 @@ nonogram(RConstr, CConstr, Rows) :-
     matrix(NRows, NCols, Rows, Cols),
     append(RConstr, CConstr, LineConstr),
     append(Rows, Cols, Lines),
+    vLine(Lines,LineConstr),
     solve(Lines, LineConstr).
 
 %
@@ -19,6 +20,7 @@ create_mat(N0, N, [Line|Matrix]) :-
     N0 > 0,
     N1 is N0 - 1,
     length(Line, N),
+    fd_domain(Line,[0,1]),                      % limita-se as variavies com 0 ou 1
     create_mat(N1, N, Matrix).
 
 extract_columns(NCols, Rows, Cols) :-
@@ -35,32 +37,40 @@ extract(Rows, Col, N) :-
     maplist(nth1(N), Rows, Col).
 
 %
-% Algoritmo para resolver o nonograma
+% Predicado para fazer as constrange
 %
+solve([],[]).
+solve([Line|TL],[CLine|TCL]):-
+    fd_labeling(Line),
+    check_line(Line,CLine),
+    solve(TL,TCL).
 
-solve(Lines, Constrs) :-
-    allPossibilitis(Lines, Constrs, All),     % todas as possibilidades por linha/coluna
-    sort(All, Sorted),!,                        % ordena por ordem de menores possibilidades
-    solve(Sorted).                            % verefica se a linha esta correta relativamente as possibilidades
+sumListaC([],0).            % fazer o contrang de uma lista para terem a soma pretendida
+sumListaC(L,Sum):-
+    sumListaC(L,0,Sum).
+sumListaC([],X,Y):-
+    X #= Y.
+sumListaC([E|L],S,Sum):-
+    S1 #= S+E,
+    sumListaC(L,S1,Sum).
 
-solve([]):-!.
-solve([line(_, Line, Constr)|Rest]) :-
-    check_line(Line, Constr),
-    solve(Rest).
+sumLista([],0).            % calcular a soma dos elemetos de uma lista
+sumLista(L,Sum):-
+    sumLista(L,0,Sum).
+sumLista([],X,X).
+sumLista([E|L],S,Sum):-
+    S1 = S+E,
+    sumLista(L,S1,Sum).
 
-%
-%   calcula todas a possibilidades uma linha segundo as restricoes e devolve uma lista de tupulos
-%   cada tupulo com o numero de possibilidades para essa linha, linha e contricao criada
-%
+vLine([],[]).                % verificar a soma de cada linha
+vLine([SubL|T],[SubL2|T2]):-
+    avSum(SubL,SubL2,_),
+    vLine(T,T2).
 
-allPossibilitis([], [], []).
-allPossibilitis([Line|Lines], [Constr|Constrs], [line(Count, Line, Constr)|Result]) :-
-    length(Line, LineLength),
-    length(CheckLine, LineLength),
-    setof(CheckLine, check_line(CheckLine, Constr), NCheckLine),
-    length(NCheckLine, Count),
-    allPossibilitis(Lines, Constrs, Result).
-
+avSum([],[],0).             % averiguar que a soma de uma lista de constrag e igual a soma da lista pretendida
+avSum(Line,CLine,S):-
+    sumLista(Line,S),
+    sumListaC(CLine,S).
 
 % check_line verefica se a linha esta correta segundo as constricoes ou constroi uma linha segundo as restricoes
 %   Exemplo:
@@ -82,33 +92,42 @@ check_line(Line, [Part|[]]) :-
     add_space(Line3, Line4),
     check_line(Line4, []).
 
-space([' '|Line],Line).                 % adiciona um espaco
+space([0|Line],Line).                 % adiciona um espaco
 
 add_space(Line, Line).                  % verefica se precisa de um espaco
-add_space([' '|Line],RestLine) :-
+add_space([0|Line],RestLine) :-
     add_space(Line, RestLine).
 
 check_part(Line, Line, 0).              % verifica se a linha esta com as partes cprretas
-check_part(['x'|Line], Rest, N) :-
+check_part([1|Line], Rest, N) :-
     N > 0,
     N1 is N - 1,
     check_part(Line, Rest, N1).
+
 
 %
 % Predicados para fazer print
 %
 
-print_elements([]).             % print de um elemento
+print_elements([]).
 print_elements([Element|T]):-
-    Element=' ', write(' .'),print_elements(T).
+    Element=0, write(' .'),print_elements(T).
 print_elements([Element|T]):-
-    Element='x', write(' X'),print_elements(T).
+    Element=1, write(' X'),print_elements(T).
 
 print_rows([]).               % print da matrix
 print_rows([H|T]) :- print_elements(H), nl, print_rows(T).
 
 %
-% Tests and examples
+% Main
+%
+
+puzzle([L,C]):-
+    nonogram(L,C,V),
+    print_rows(V).
+
+%
+% Testes
 %
 
 test1 :-
@@ -153,7 +172,3 @@ test7 :-
      RowConstr = [[5], [2, 3, 2], [2, 5, 1], [2, 8], [2, 5, 11], [1, 1, 2, 1, 6], [1, 2, 1, 3], [2, 1, 1], [2, 6, 2], [15, 4], [10, 8], [2, 1, 4, 3, 6], [17], [17], [18], [1, 14], [1, 1, 14], [5, 9], [8], [7]],
      ColConstr = [[5], [3, 2], [2, 1, 2], [1, 1, 1], [1, 1, 1], [1, 3], [2, 2], [1, 3, 3], [1, 3, 3, 1], [1, 7, 2], [1, 9, 1], [1, 10], [1, 10], [1, 3, 5], [1, 8], [2, 1, 6], [3, 1, 7], [4, 1, 7], [6, 1, 8], [6, 10], [7, 10], [1, 4, 11], [1, 2, 11], [2, 12], [3, 13]],
      puzzle([RowConstr, ColConstr]).
-
-puzzle([RowConstr, ColConstr|_]) :-
-    nonogram(RowConstr, ColConstr, Rows),
-    nl, print_rows(Rows), nl.
